91eada7cfc6ff143f1869338364fb4e3e73e9a43
lizbeth124 <144714317+lizbeth124@users.noreply.github.com>
2023-10-15 22:18:09 -0400

    Add files via upload

---
+++ Milestone 5.R
@@ -0,0 +1,12 @@
+#Statistics for Column "Number of Students"
+mean(rank$`No of student`, trim = 0.10, na.rm = TRUE)
+median(rank$`No of student`, na.rm = TRUE)
+var(rank$`No of student`, na.rm = TRUE)
+sd(rank$`No of student`, na.rm = TRUE)
+
+#Statistics for Column "Number of Students per staff"
+mean(rank$`No of student per staff`, trim = 0.10, na.rm = TRUE)
+median(rank$`No of student per staff`, na.rm = TRUE)
+var(rank$`No of student per staff`, na.rm = TRUE)
+sd(rank$`No of student per staff`, na.rm = TRUE)
+
